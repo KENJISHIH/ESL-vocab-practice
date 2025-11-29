73e745bbbea68de4a3b8f2a4cb4a1ff0f1f11aed
KENJISHIH <126316725+KENJISHIH@users.noreply.github.com>
2025-11-29 14:50:01 +0800

    Add files via upload

---
+++ week14_data.js
@@ -0,0 +1,64 @@
+// 這是 Week 14 的單字資料庫
+const week14Data = [
+    {
+        word: "toucan",
+        pos: "(n.)",
+        def: "a big, brightly colored bird with a large beak",
+        ex: "1. Toucans live in tropical America.<br>2. We can see toucans in the zoo."
+    },
+    {
+        word: "find out",
+        pos: "(v.)",
+        def: "to learn something by asking or studying",
+        ex: "1. Please find out the mistake on your worksheet.<br>2. Let's find out the answer together."
+    },
+    {
+        word: "branch",
+        pos: "(n.)",
+        def: "a part of a tree growing from the trunk",
+        ex: "1. I climbed up the tree and sat on a branch.<br>2. When I throw a little branch, my dog catches it."
+    },
+    {
+        word: "poisonous",
+        pos: "(adj.)",
+        def: "something that can kill you if you get it in your body",
+        ex: "1. A rattlesnake is a poisonous animal.<br>2. Snow White ate a poisonous apple."
+    },
+    {
+        word: "iguana",
+        pos: "(n.)",
+        def: "a kind of lizard",
+        ex: "1. James wants an iguana for a pet.<br>2. I see a red iguana on the ground."
+    },
+    {
+        word: "caterpillar",
+        pos: "(n.)",
+        def: "an insect like a worm with many legs",
+        ex: "1. A caterpillar will change into a butterfly.<br>2. There is a caterpillar on the branch."
+    },
+    {
+        word: "asleep",
+        pos: "(adj.)",
+        def: "not awake; sleeping",
+        ex: "1. The baby fell asleep soon.<br>2. He was so tired that he fell asleep on the sofa."
+    },
+    {
+        word: "another",
+        pos: "(adj.)",
+        def: "one more; an additional thing or person",
+        ex: "1. We will talk about it in another day.<br>2. Sam lost his pen and borrowed one from another boy."
+    },
+    // Science Category
+    {
+        word: "hatch",
+        pos: "(v.)",
+        def: "(of an egg) to break, letting the young bird out",
+        ex: "1. Three chicks have hatched this morning.<br>2. Hens hatch their eggs by keeping them warm."
+    },
+    {
+        word: "tadpole",
+        pos: "(n.)",
+        def: "a baby frog",
+        ex: "1. Tadpoles grow into frogs.<br>2. Look! The tadpoles are swimming in the pool."
+    }
+];
