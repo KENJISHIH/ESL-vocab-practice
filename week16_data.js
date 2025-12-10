6036f0d147c1f4c1dac73028dbe08d31e609eeb8
KENJISHIH <126316725+KENJISHIH@users.noreply.github.com>
2025-12-10 10:59:47 +0800

    Add files via upload

---
+++ week16_data.js
@@ -0,0 +1,64 @@
+// 這是 Week 16 的單字資料庫
+const week16Data = [
+    {
+        word: "shade",
+        pos: "(n.)",
+        def: "a dark area where the light from the Sun does not reach because it is blocked by something",
+        ex: "1. He sits under the shade of a tree.<br>2. I feel hot in the sun; let’s get into the shade."
+    },
+    {
+        word: "lemonade",
+        pos: "(n.)",
+        def: "a drink made from lemons, sugar, and water",
+        ex: "1. I want to add some honey in my lemonade.<br>2. My mother buys lemonade in the supermarket."
+    },
+    {
+        word: "both",
+        pos: "(pron.)",
+        def: "two people or things together",
+        ex: "1. Both of my sisters live in Taichung.<br>2. After working, both of us were tired."
+    },
+    {
+        word: "crayon",
+        pos: "(n.)",
+        def: "a stick of colored wax used for drawing",
+        ex: "1. Dad gave me a new box of crayons.<br>2. I forgot to bring my crayons. May I borrow yours?"
+    },
+    {
+        word: "same",
+        pos: "(adj.)",
+        def: "not different",
+        ex: "1. We both take the same bus to school.<br>2. My father sits in the same chair every evening."
+    },
+    {
+        word: "road",
+        pos: "(n.)",
+        def: "a big street",
+        ex: "1. Watch out for cars when you cross the road.<br>2. There is a road in front of my school."
+    },
+    {
+        word: "mine",
+        pos: "(pron.)",
+        def: "something that is yours",
+        ex: "1. That doll is mine. You can’t take it away.<br>2. Is this book mine?"
+    },
+    {
+        word: "toad",
+        pos: "(n.)",
+        def: "an animal like a big frog",
+        ex: "1. Toads have shorter legs than frogs.<br>2. Don’t touch that toad! It’s poisonous."
+    },
+    // Science Category
+    {
+        word: "ocean",
+        pos: "(n.)",
+        def: "a very large area of salty water; the sea",
+        ex: "1. Whales live in the ocean.<br>2. Many mammals, fish and plants live in the ocean."
+    },
+    {
+        word: "fresh",
+        pos: "(adj.)",
+        def: "new, clean and cool ; plain, not salty or sweet",
+        ex: "1. Fresh water has little or no salt in it.<br>2. This kind of fish lives in fresh water."
+    }
+];
