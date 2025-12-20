79dacb56157e010d737652ea54cd8cf4e83418a3
KENJISHIH <126316725+KENJISHIH@users.noreply.github.com>
2025-12-20 20:53:41 +0800

    Add files via upload

---
+++ week17_data.js
@@ -0,0 +1,64 @@
+// 這是 Week 17 的單字資料庫
+const week17Data = [
+    {
+        word: "down",
+        pos: "(adv.)",
+        def: "to or towards a lower place",
+        ex: "1. Put your bag down on the floor.<br>2. Danny jumped down from the tree."
+    },
+    {
+        word: "need",
+        pos: "(v.)",
+        def: "when there is something you must have",
+        ex: "1. We need to clean up the toys before bedtime.<br>2. I don’t need to take medicine anymore."
+    },
+    {
+        word: "mad",
+        pos: "(adj.)",
+        def: "be angry at",
+        ex: "1. I am mad at John because he took my cookies.<br>2. The puppy looks mad when it’s hungry."
+    },
+    {
+        word: "fault",
+        pos: "(n.)",
+        def: "a mistake",
+        ex: "1. The broken vase was my fault.<br>2. It’s not your fault if the blocks fall down."
+    },
+    {
+        word: "quick",
+        pos: "(adv.)",
+        def: "in a fast way; not slow",
+        ex: "1. Quick! Eat your ice-cream or it will melt.<br>2. Howard runs as quick as a horse."
+    },
+    {
+        word: "stone",
+        pos: "(n.)",
+        def: "a small rock",
+        ex: "1. Grace picked up a stone and threw it into the lake.<br>2. We found many stones along the river."
+    },
+    {
+        word: "until",
+        pos: "(conj.)",
+        def: "up to the time",
+        ex: "1. Peter waited in the office until his parents came.<br>2. Judy watched TV until eleven o’clock."
+    },
+    {
+        word: "hard",
+        pos: "(adj.)",
+        def: "not easy to break; not easy",
+        ex: "1. The man gave the tree trunk a hard kick.<br>2. This question is too hard for a 5-year-old child."
+    },
+    // Science Category
+    {
+        word: "food chain",
+        pos: "(n.)",
+        def: "the order in which living things get food in a habitat",
+        ex: "1. All animals are part of a food chain.<br>2. Insects are fairly low in the food chain."
+    },
+    {
+        word: "extinct",
+        pos: "(adj.)",
+        def: "all of one kind of animal or plant dies",
+        ex: "1. Dinosaurs are extinct.<br>2. When the habitat changed, some animals became extinct."
+    }
+];
